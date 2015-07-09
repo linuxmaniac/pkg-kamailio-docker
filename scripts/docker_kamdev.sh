d61aabec55a269ff6c6f4f51a4044fc98214c970
Victor Seva <linuxmaniac@torreviejawireless.org>
2015-07-09 22:56:05 +0200

    scripts/docker_kamdev.sh: add stretch too

--- scripts/docker_kamdev.sh
+++ scripts/docker_kamdev.sh
@@ -108,7 +108,7 @@ done
 [ -z "${KAM_CONF_DIR}" ] && KAM_CONF_DIR="${BASE_GIT_DIR}/kamailio_dev"
 
 case "${DIST}" in
-  jessie|wheezy|squeeze)
+  stretch|jessie|wheezy|squeeze)
     img="${IMG_BASE}:${DIST}"
     ;;
   *)
