3d787010acdcc4f6059b1bd8f49fceb811b9e1d9
Victor Seva <linuxmaniac@torreviejawireless.org>
2015-11-24 00:27:38 +0100

    add clang support
    - clang-3.4 for travis builds
    - REFRESHED_AT

--- create_dockerfile.sh
+++ create_dockerfile.sh
@@ -4,9 +4,15 @@ create_dockerfile() {
   cat > "${dist}/Dockerfile" <<EOF
 FROM debian:${dist}
 
+# Important! Update this no-op ENV variable when this Dockerfile
+# is updated with the current date. It will force refresh of all
+# of the base images and things like `apt-get update` won't be using
+# old cached versions when the Dockerfile is built.
+ENV REFRESHED_AT ${DATE}
+
 RUN rm -rf /var/lib/apt/lists/* && apt-get update
 RUN apt-get install --assume-yes \
-  pbuilder mysql-client gdb screen sip-tester sipsak psmisc joe lynx less
+  pbuilder mysql-client gdb screen sip-tester sipsak psmisc joe lynx less clang-3.4
 
 VOLUME /code
 
@@ -26,6 +32,8 @@ EOF
 }
 
 dist=${1:-sid}
+
+DATE=$(date --rfc-3339=date)
 
 if [ -d src ] ; then
 	(cd src && git pull)
